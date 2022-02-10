4a32728588bf30514cc78dd4d1d626105f26679e
Leonardo Margoti Meister <leozinhomm1@gmail.com>
2022-02-10 15:26:00 -0300

    Atualizacao

--- Robo2022/Program.cs
+++ Robo2022/Program.cs
@@ -6,15 +6,15 @@ namespace Robo2022
     {
         static void Main(string[] args)
         {
+            //-----------------------------------------VARIAVEIS DE USO DO PROGRAMA
             string[] mapa = new string[2];
             string comandorobo1, comandorobo2, robo2Dire, robo1Dire;
             int robo1x, robo1y, robo2x, robo2y;
             String[] separarXYZ;
-
-
+            //-----------------------------------------PEGANDO OS DADOS DO MAPA
             Console.WriteLine("Informe as coordenadas do mapa: Ex(4 4)");
             mapa = Console.ReadLine().ToLower().Split();
-
+            //-----------------------------------------PEGANDO DADOS DO ROBO 1
             Console.WriteLine("Informe a coordenada do robo 1: Ex(2 2 L)");
             separarXYZ = Console.ReadLine().Split();
             robo1x = Convert.ToInt32((String)separarXYZ[0]);
@@ -22,7 +22,7 @@ namespace Robo2022
             robo1Dire = (String)separarXYZ [2];
             Console.WriteLine("Informe o Comando do Robo: Ex(MMEDDM)");
             comandorobo1 = Console.ReadLine().ToLower();
-            
+            //-----------------------------------------PEGANDO DADOS DO ROBO 2
             Console.WriteLine("Informe a coordenada do robo 2: Ex(2 2 L)");
             separarXYZ = Console.ReadLine().Split();
             robo2x = Convert.ToInt32((String)separarXYZ[0]);
@@ -30,7 +30,7 @@ namespace Robo2022
             robo2Dire = (String)separarXYZ[2];
             Console.WriteLine("Informe o Comando do Robo: Ex(MMEDDM)");
             comandorobo2 = Console.ReadLine().ToLower();
-
+            //-----------------------------------------SEGUINDO OS COMANDOS DO ROBO 1
             for (int i = 0; i < comandorobo1.Length; i++)
             {
                 if (comandorobo1[i] == 'd')
@@ -55,7 +55,7 @@ namespace Robo2022
                     else if (robo1Dire == "o") robo1x--;
                 }
             }
-
+            //-----------------------------------------SEGUINDO OS COMANDOS DO ROBO 2
             for (int i = 0; i < comandorobo2.Length; i++)
             {
                 if (comandorobo2[i] == 'd')
@@ -80,10 +80,9 @@ namespace Robo2022
                     else if (robo2Dire == "o") robo2x--;
                 }
             }
-
+            //-----------------------------------------SAIDA DE DADOS DO SISTEMA
             Console.WriteLine($"{robo1x} {robo1y} {robo1Dire}");
             Console.WriteLine($"{robo2x} {robo2y} {robo2Dire}");
-            Console.ReadKey();
         }
     }
 }
